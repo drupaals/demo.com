ff567c6d51686c5dc3c3e3b683413704160666ec
Rohit kumar <rohit@rhaasoft.com>
2014-05-07 17:29:27 +0530

    udpated for star rating

--- sites/all/themes/ace/views-view-fields--comments.tpl.php
+++ sites/all/themes/ace/views-view-fields--comments.tpl.php
@@ -24,7 +24,7 @@
  * @ingroup views_templates
  */
 
-
+     drupal_add_css(drupal_get_path('module','fivestar').'/css/fivestar.css');
         $comment_load=comment_load($row->comment_node_cid);
 	  $user_profile=user_load($comment_load->uid);
 	  //$created_date=date('j F Y h:i:s',$comment_load->created);
@@ -34,6 +34,22 @@
 	  }else{
 	       $user_img=theme("image", array('path' => path_to_theme().'/images/user.png',  'alt'=>$user_profile->name, 'title'=>$user_profile->name,  'width' => 48,    'height' => 48));
 	  }
+          if(!empty($comment_load->field_rating)){
+	       if($comment_load->field_rating['und'][0]['rating'] == 20){
+		    $rating='<div class="fivestar-default"><div class="fivestar-widget-static fivestar-widget-static-vote fivestar-widget-static-5 clearfix"><div class="star star-1 star-odd star-first"><span class="on">1</span></div><div class="star star-2 star-even"><span class="off"></span></div><div class="star star-3 star-odd"><span class="off"></span></div><div class="star star-4 star-even"><span class="off"></span></div><div class="star star-5 star-odd star-last"><span class="off"></span></div></div></div>';
+	       }elseif($comment_load->field_rating['und'][0]['rating'] == 40){
+		    $rating='<div class="fivestar-default"><div class="fivestar-widget-static fivestar-widget-static-vote fivestar-widget-static-5 clearfix"><div class="star star-1 star-odd star-first"><span class="on">2</span></div><div class="star star-2 star-even"><span class="on"></span></div><div class="star star-3 star-odd"><span class="off"></span></div><div class="star star-4 star-even"><span class="off"></span></div><div class="star star-5 star-odd star-last"><span class="off"></span></div></div></div>';
+	       }elseif($comment_load->field_rating['und'][0]['rating'] == 60){
+		    $rating='<div class="fivestar-default"><div class="fivestar-widget-static fivestar-widget-static-vote fivestar-widget-static-5 clearfix"><div class="star star-1 star-odd star-first"><span class="on">3</span></div><div class="star star-2 star-even"><span class="on"></span></div><div class="star star-3 star-odd"><span class="on"></span></div><div class="star star-4 star-even"><span class="off"></span></div><div class="star star-5 star-odd star-last"><span class="off"></span></div></div></div>';
+	       }elseif($comment_load->field_rating['und'][0]['rating'] == 80){
+		    $rating='<div class="fivestar-default"><div class="fivestar-widget-static fivestar-widget-static-vote fivestar-widget-static-5 clearfix"><div class="star star-1 star-odd star-first"><span class="on">4</span></div><div class="star star-2 star-even"><span class="on"></span></div><div class="star star-3 star-odd"><span class="on"></span></div><div class="star star-4 star-even"><span class="on"></span></div><div class="star star-5 star-odd star-last"><span class="off"></span></div></div></div>';
+	       }
+	       elseif($comment_load->field_rating['und'][0]['rating'] == 100){
+		   $rating='<div class="fivestar-default"><div class="fivestar-widget-static fivestar-widget-static-vote fivestar-widget-static-5 clearfix"><div class="star star-1 star-odd star-first"><span class="on">5</span></div><div class="star star-2 star-even"><span class="on"></span></div><div class="star star-3 star-odd"><span class="on"></span></div><div class="star star-4 star-even"><span class="on"></span></div><div class="star star-5 star-odd star-last"><span class="on"></span></div></div></div>'; 
+	       }
+	       
+	  }else{$rating='<div class="fivestar-widget clearfix fivestar-widget-5"><div class="star star-1 odd star-first"><a title="Give it 1/5" href="#20">Give it 1/5</a></div><div class="star star-2 even"><a title="Give it 2/5" href="#40">Give it 2/5</a></div><div class="star star-3 odd"><a title="Give it 3/5" href="#60">Give it 3/5</a></div><div class="star star-4 even"><a title="Give it 4/5" href="#80">Give it 4/5</a></div><div class="star star-5 odd star-last"><a title="Give it 5/5" href="#100">Give it 5/5</a></div></div>';}
+	  
         $allcomments='<div class="itemdiv dialogdiv">
                   <div class="user">
                        '.$user_img.'
@@ -44,7 +60,9 @@
                                   <i class="icon-time"></i>
                                   <span class="green">'.humanTiming($comment_load->created).'  ago</span>
                           </div>
-   
+                            <div class="rating">
+				   '.$rating.'
+			    </div>
                           <div class="name">
                                  '.ucfirst($comment_load->name).'
                           </div>
