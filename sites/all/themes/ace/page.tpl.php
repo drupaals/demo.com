4e89f6918a4ab638fc5ae70adc72e2e79faab212
Rohit kumar <rohit@rhaasoft.com>
2014-05-08 11:22:39 +0530

    remove icon

--- sites/all/themes/ace/page.tpl.php
+++ sites/all/themes/ace/page.tpl.php
@@ -28,29 +28,7 @@
 		<span class="menu-text"></span>
 	</a>
   <div class="sidebar" id="sidebar">
-    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
-	<div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
-	  <button class="btn btn-success">
-	     <i class="icon-signal"></i>
-	  </button>
-	  <button class="btn btn-info">
-	     <i class="icon-pencil"></i>
-	  </button>
-	  <button class="btn btn-warning">
-	    <i class="icon-group"></i>
-	  </button>
-	  <button class="btn btn-danger">
-	     <i class="icon-cogs"></i>
-	  </button>
-	</div>
-
-      <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
-	<span class="btn btn-success"></span>
-	<span class="btn btn-info"></span>
-	<span class="btn btn-warning"></span>
-	<span class="btn btn-danger"></span>
-      </div>
-    </div>
+   
     <!-- #sidebar-shortcuts -->
     <?php if ($page['left_sidebar']): print render($page['left_sidebar']);  endif; ?> 
       <div class="sidebar-collapse" id="sidebar-collapse">
