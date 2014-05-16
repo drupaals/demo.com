ac9fc271dc8c16eb5b4cc821b9f41468ffc066f8
Rohit kumar <rohit@rhaasoft.com>
2014-05-16 10:23:38 +0530

    updated for review comment

--- sites/all/themes/ace/node--module_review.tpl.php
+++ sites/all/themes/ace/node--module_review.tpl.php
@@ -157,7 +157,7 @@ $output='<div class="details-info">
 
 
 		<div id="myModal" class="reveal-modal">
-		 <div id="anno-user">'.l('Login', 'user' , array('html' => TRUE)).'  |  '.l('Register', 'user/register' , array('html' => TRUE)).'</div>
+		 <div id="anno-user" style="text-align: center;">'.l('Login', 'user' , array('query' => array('destination' => drupal_get_destination()['destination']))).'  |  '.l('Register', 'user/register' , array('query' => array('destination' => drupal_get_destination()['destination']))).'</div>
     
 		</div>';}
     $output_description='<div class="details-section description simple contains-text-link"><hr><h3>Description</h3><div>'.$body.'</div></div><hr>';
