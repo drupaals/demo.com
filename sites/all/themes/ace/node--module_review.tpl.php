af15389db666d8cdfdaca914d4c72d2aef751e15
Rohit kumar <rohit@rhaasoft.com>
2014-06-13 12:26:06 +0530

    For bug

--- sites/all/themes/ace/node--module_review.tpl.php
+++ sites/all/themes/ace/node--module_review.tpl.php
@@ -180,13 +180,15 @@ if(arg(0) ==  "node"){
 	if(!empty($node->field_drupal_module_link)){
 	//Recommended releases
 	$str = file_get_contents($node->field_drupal_module_link['und'][0]['value'].'/release/feed');
-	$substr=substr(explode("view-project-release-files",$str)[1], 173);
-	//Other releases
-	$substr1=substr(explode("view-project-release-files",$str)[2], 173);
-	$drupal_download='<div class="drupal-download"><h4>Recommended releases</h4>'.htmlspecialchars_decode(substr($substr, 0,2515)).'</div>
-	
+	    if($substr == ""){
+		$substr=substr(explode("view-project-release-files",$str)[1], 173);
+		//Other releases
+		$substr1=substr(explode("view-project-release-files",$str)[2], 173);
+		$drupal_download='<div class="drupal-download"><h4>Recommended releases</h4>'.htmlspecialchars_decode(substr($substr, 0,2515)).'</div>
+	    
 			 </ br></ br><div class="drupal-dev-download" style="margin-top: 25px;"><h4>Other releases</h4>'.htmlspecialchars_decode(substr($substr1, 0,2515)).'
 			 <div class="see-all-releases"><h4 style="text-decoration: underline; color: rgb(66, 139, 202);">'.l('See all releases', $node->field_drupal_module_link['und'][0]['value'].'/release', array('attributes'=>array('target'=>'_blank'))).'</h4></div><hr>';
+	    }else{$drupal_download="";}
 	}
 	$output_review='<div class="details-section description simple contains-text-link"><h4>Reviews '.$comment_link.'</h4>
 				 <div id="module-comment">
