8be19a9d9fbdbd819ddc4834f6cc9d41e80060ea
Rohit kumar <rohit@rhaasoft.com>
2014-06-13 11:54:29 +0530

    For bug

--- sites/all/themes/ace/node--module_review.tpl.php
+++ sites/all/themes/ace/node--module_review.tpl.php
@@ -190,8 +190,8 @@ if(arg(0) ==  "node"){
 	/*$drupal_download='<div class="drupal-download"><h4>Recommended releases</h4>'.htmlspecialchars_decode(substr($substr, 0,2515)).'</div>
 	
 			 </ br></ br><div class="drupal-dev-download" style="margin-top: 25px;"><h4>Other releases</h4>'.htmlspecialchars_decode(substr($substr1, 0,2515)).'
-			 <div class="see-all-releases"><h4 style="text-decoration: underline; color: rgb(66, 139, 202);">'.l('See all releases', $node->field_drupal_module_link['und'][0]['value'].'/release', array('attributes'=>array('target'=>'_blank'))).'</h4></div><hr>';
-	}*/
+			 <div class="see-all-releases"><h4 style="text-decoration: underline; color: rgb(66, 139, 202);">'.l('See all releases', $node->field_drupal_module_link['und'][0]['value'].'/release', array('attributes'=>array('target'=>'_blank'))).'</h4></div><hr>';*/
+	}
 	$output_review='<div class="details-section description simple contains-text-link"><h4>Reviews '.$comment_link.'</h4>
 				 <div id="module-comment">
 				'.render($content['comments']).'
