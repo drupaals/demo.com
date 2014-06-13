d1aed5edcea45e490c98355687493e321563f514
Rohit kumar <rohit@rhaasoft.com>
2014-06-13 12:27:21 +0530

    For bug

--- sites/all/themes/ace/node--module_review.tpl.php
+++ sites/all/themes/ace/node--module_review.tpl.php
@@ -180,7 +180,7 @@ if(arg(0) ==  "node"){
 	if(!empty($node->field_drupal_module_link)){
 	//Recommended releases
 	$str = file_get_contents($node->field_drupal_module_link['und'][0]['value'].'/release/feed');
-	    if($substr == ""){
+	    if($str == ""){
 		$substr=substr(explode("view-project-release-files",$str)[1], 173);
 		//Other releases
 		$substr1=substr(explode("view-project-release-files",$str)[2], 173);
